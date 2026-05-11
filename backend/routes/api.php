40fda32d34dfa034e66bdfc08f7e45d4c72480e7
tatyloem1u <navec-saron.taty-loemba8@etu.univ-lorraine.fr>
2026-05-11 15:57:43 +0200

    Admin_interface: Ajout de useCategories

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,62 +9,63 @@ use App\Http\Controllers\EventPhotoController;
 use App\Http\Controllers\CategoryController;
 
 /*
---------------------------------------------------------------------------
- ROUTES PUBLIQUES (accessibles à tous)
---------------------------------------------------------------------------
+|--------------------------------------------------------------------------
+| ROUTES PUBLIQUES (avec session web)
+|--------------------------------------------------------------------------
 */
+Route::middleware(['web'])->group(function () {
+    
+    //AUTH
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 
-//Authentification
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+    //PASSWORD
+    Route::post('/forgot-password', [PasswordResetController::class, 'forgotPassword']);
+    Route::post('/reset-password', [PasswordResetController::class, 'resetPassword']);
 
-//PASSWORD
-Route::post('/forgot-password', [PasswordResetController::class, 'forgotPassword']);
-Route::post('/reset-password', [PasswordResetController::class, 'resetPassword']);
-
-//CATÉGORIES (lecture publique)
-Route::get('/categories', [CategoryController::class, 'index']);
+    //CATÉGORIES (lecture publique)
+    Route::get('/categories', [CategoryController::class, 'index']);
+});
 
 
 /*
 |--------------------------------------------------------------------------
-ROUTES PROTÉGÉES (utilisateurs connectés)
+| ROUTES PROTÉGÉES (web + sanctum)
 |--------------------------------------------------------------------------
 */
-
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['web', 'auth:sanctum'])->group(function () {
 
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    //EVENTS (lecture connectée)
+    // EVENTS (lecture connectée)
     Route::get('/events', [EventController::class, 'index']);
     Route::get('/events/{id}', [EventController::class, 'show']);
 
     /*
     |--------------------------------------------------------------------------
-     ADMIN ONLY
+    |  ADMIN ONLY (web + sanctum + admin)
     |--------------------------------------------------------------------------
     */
     Route::middleware('admin')->group(function () {
 
-        //CRUD Utilisateurs
+        // CRUD Utilisateurs
         Route::get('/users', [UserController::class, 'index']);
         Route::get('/users/{id}', [UserController::class, 'show']);
         Route::put('/users/{id}', [UserController::class, 'update']);
         Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
-        //CRUD Événements
+        // CRUD Événements
         Route::post('/events', [EventController::class, 'store']);
         Route::put('/events/{id}', [EventController::class, 'update']);
         Route::delete('/events/{id}', [EventController::class, 'destroy']);
         Route::post('/events/{id}/categories', [EventController::class, 'addCategories']);
 
-        //Catégories (création/suppression)
+        // Catégories (création/suppression)
         Route::post('/categories', [CategoryController::class, 'store']);
         Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
-        //Photos
+        // Photos
         Route::post('/events/{id}/photos', [EventPhotoController::class, 'store']);
         Route::delete('/photos/{id}', [EventPhotoController::class, 'destroy']);
 
