e68ce2f33b3148cbad7d4254719eba2146d645aa
tatyloem1u <navec-saron.taty-loemba8@etu.univ-lorraine.fr>
2026-05-11 15:28:42 +0200

    Admin_interface: update routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,27 +9,26 @@ use App\Http\Controllers\EventPhotoController;
 use App\Http\Controllers\CategoryController;
 
 /*
-|--------------------------------------------------------------------------
-| ROUTES PUBLIQUES
-|--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+ ROUTES PUBLIQUES (accessibles à tous)
+--------------------------------------------------------------------------
 */
 
-// AUTH
+//Authentification
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// PASSWORD
+//PASSWORD
 Route::post('/forgot-password', [PasswordResetController::class, 'forgotPassword']);
 Route::post('/reset-password', [PasswordResetController::class, 'resetPassword']);
 
-// EVENTS PUBLICS
-Route::get('/events', [EventController::class, 'index']);
-Route::get('/events/{id}', [EventController::class, 'show']);
+//CATÉGORIES (lecture publique)
+Route::get('/categories', [CategoryController::class, 'index']);
 
 
 /*
 |--------------------------------------------------------------------------
-| ROUTES PROTÉGÉES
+ROUTES PROTÉGÉES (utilisateurs connectés)
 |--------------------------------------------------------------------------
 */
 
@@ -38,34 +37,40 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    // ADMIN ONLY
+    //EVENTS (lecture connectée)
+    Route::get('/events', [EventController::class, 'index']);
+    Route::get('/events/{id}', [EventController::class, 'show']);
+
+    /*
+    |--------------------------------------------------------------------------
+     ADMIN ONLY
+    |--------------------------------------------------------------------------
+    */
     Route::middleware('admin')->group(function () {
 
-        // CRUD Utilisateurs
+        //CRUD Utilisateurs
         Route::get('/users', [UserController::class, 'index']);
         Route::get('/users/{id}', [UserController::class, 'show']);
         Route::put('/users/{id}', [UserController::class, 'update']);
         Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
-        // CRUD Événements
+        //CRUD Événements
         Route::post('/events', [EventController::class, 'store']);
         Route::put('/events/{id}', [EventController::class, 'update']);
         Route::delete('/events/{id}', [EventController::class, 'destroy']);
+        Route::post('/events/{id}/categories', [EventController::class, 'addCategories']);
 
-        // Catégories
-        Route::get('/categories', [CategoryController::class, 'index']);
+        //Catégories (création/suppression)
         Route::post('/categories', [CategoryController::class, 'store']);
         Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
-        // Photos
+        //Photos
         Route::post('/events/{id}/photos', [EventPhotoController::class, 'store']);
         Route::delete('/photos/{id}', [EventPhotoController::class, 'destroy']);
 
-        // TEST ADMIN
+        //TEST ADMIN
         Route::get('/admin', function () {
-            return response()->json([
-                'message' => 'Bienvenue admin'
-            ]);
+            return response()->json(['message' => 'Bienvenue admin']);
         });
     });
 });
